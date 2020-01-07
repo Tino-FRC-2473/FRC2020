768edd183808503221c79f5a3dbc2d5261c107ab
imsanika03 <imsanika03@gmail.com>
2020-01-06 20:29:40 -0800

    Initial Teleop commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,7 +27,10 @@ public class RobotContainer {
 
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 * 
 	 */
+
+	Joystick j1 = new Joystick(1);
 	public RobotContainer() {
 		// Configure the button bindings
 		configureButtonBindings();
